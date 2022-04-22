55fb3f8dc802a1d183b0c162eadf09fcbe41a0a0
blid11 <blid11@DESKTOP-727Q0Q7.localdomain>
2022-04-22 15:51:48 +1000

    testing commit to aws again

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <h1>Hello from aws</h1>
       </header>
     </div>
   );
